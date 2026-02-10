db80b8758c92aa2c0244d705f6e8cbca6aacb882
kursaddcinar <kursaddcinar@gmail.com>
2026-02-10 18:10:14 +0300

    chore(backend): Initialize Spring Boot backend skeleton

---
+++ hms-backend/src/test/java/com/kursaddcinar/hms_backend/HmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursaddcinar.hms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
